9f667a43768fef98c0115a3fdf54c6a231644da6
Ragini <raginimallah.rabs@gmail.com>
2026-04-27 11:58:44 +0530

    changes

--- Backend/routes/followups.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { authenticate } = require('../middleware/auth');
-const followupController = require('../controllers/followupController');
-
-router.use(authenticate);
-router.get('/', followupController.getFollowups);
-
-module.exports = router;
